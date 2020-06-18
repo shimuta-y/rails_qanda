62527976e4f905792a58f00852b16f71241700d5
EC2 Default User <ec2-user@ip-172-31-14-212.ap-northeast-1.compute.internal>
2020-06-18 15:49:16 +0000

    routesの設定変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  get 'questions/index'
-  get 'questions/show'
-  get 'questions/new'
-  get 'questions/edit'
+  # get 'questions/index'
+  # get 'questions/show'
+  # get 'questions/new'
+  # get 'questions/edit'
+  
+  resources :questions
+  # index => 質問一覧
+  # create => 新規作成の時の処理
+  # new => 質問の新規登録
+  # edit => 編集
+  # show => 詳細
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
