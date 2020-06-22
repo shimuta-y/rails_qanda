c5ccfb068f4ed70ad3e8e1272f7ae522ad475b09
EC2 Default User <ec2-user@ip-172-31-14-212.ap-northeast-1.compute.internal>
2020-06-22 17:10:15 +0000

    DBに反映しないバグの修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,35 @@
 class QuestionsController < ApplicationController
+
   def index
     @questions = Question.all
   end
 
   def show
+    # @question = Question.find(params[:id])
+    # @answer = Answer.new
   end
 
   def new
     @question = Question.new
   end
+  
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path, notice: 'Success!'
+    else
+      flash[:alert] = 'Save error!'
+      # render :new
+    end
+  end
 
   def edit
+    # @question = Question.find(params[:id])
   end
+  
+  private
+    def question_params
+      params.require(:question).permit(:name, :title, :content)
+    end
+  
 end
